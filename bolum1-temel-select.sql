bc327fdeaca732f5bb3b807ea14e03674f61ccbf
thinkoptimize <thinkoptimize@yandex.com>
2025-03-24 12:59:02 +0300

    bolum1-temel-select.sql

--- bolum1-temel-select.sql
+++ bolum1-temel-select.sql
@@ -1,3 +1,6 @@
+Bölüm Özeti:
+Bu yazıda ele aldığımız temel sorgular, T-SQL dilinin yapı taşlarını öğrenmek için başlangıç noktasıdır. SELECT komutu ile başlayan temel sorgular; filtreleme (WHERE), sıralama (ORDER BY), veri sınırlama (TOP), farklılık (DISTINCT), desen eşleşmesi (LIKE) ve toplulaştırma (COUNT) gibi özelliklerle zenginleştirilebilir. Bu kavramlar, sonraki seviyedeki T-SQL tekniklerinin de temelini oluşturacaktır.
+
 
  1. Tüm Çalışanların Ad ve Soyadlarını Listeleyin
 
@@ -55,3 +58,39 @@ FROM Production.Product
 WHERE ListPrice BETWEEN 500 AND 1000;
 
 BETWEEN operatörü, belirtilen iki değer arasındaki kayıtları getirir. Hem alt hem de üst sınır dahildir.
+ ___________________________________________________________________________________________________________
+
+7.Adresi Olmayan (NULL) Kişileri Getirin
+
+SELECT p.FirstName, p.LastName
+FROM Person.Person p
+LEFT JOIN Person.BusinessEntityAddress bea ON p.BusinessEntityID = bea.BusinessEntityID
+WHERE bea.AddressID IS NULL;
+
+Bu sorgu, LEFT JOIN kullanılarak ilişkilendirme yapılmış fakat adres bilgisi olmayan kişileri bulur. IS NULL, eksik veriyi tespit etmek için kullanılır.
+
+ ___________________________________________________________________________________________________________
+
+8. 2011 Yılında Verilen Siparişleri Listeleyin
+
+SELECT SalesOrderID, OrderDate, TotalDue
+FROM Sales.SalesOrderHeader
+WHERE YEAR(OrderDate) = 2011;
+
+Bu sorguda YEAR() fonksiyonu kullanılarak tarih sütunundaki yıl bilgisi ayrıştırılır. Böylece yalnızca 2011 yılına ait siparişler filtrelenir.
+
+ ___________________________________________________________________________________________________________
+
+ 9. Çalışanların Ünvanlarını Getirin (Tekrar Etmeyen)
+ 
+SELECT DISTINCT JobTitle
+FROM HumanResources.Employee;
+
+Her bir çalışanın ünvanını yalnızca bir kez göstermek için DISTINCT ifadesi kullanılmıştır.
+ ___________________________________________________________________________________________________________
+10.Toplam Ürün Sayısını Hesaplayın
+
+SELECT COUNT(*) AS TotalProductCount
+FROM Production.Product;
+
+COUNT(*) fonksiyonu, satır sayısını hesaplar. AS ile sütuna takma ad verilmiştir.
