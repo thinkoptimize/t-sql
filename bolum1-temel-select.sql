62c63039c630097abaead4dd75a12693cb00f4f4
thinkoptimize <thinkoptimize@yandex.com>
2025-03-24 11:36:14 +0300

    bolum1-temel-select.sql

--- bolum1-temel-select.sql
+++ bolum1-temel-select.sql
@@ -1,7 +1,19 @@
+
+🔹 1. Tüm Çalışanların Ad ve Soyadlarını Listeleyin
+
 SELECT FirstName, LastName
 FROM Person.Person
 WHERE PersonType = 'EM';
 
-
-🎓 Açıklama:
 Bu sorgu, Person.Person tablosunda yer alan ve PersonType sütununda "EM" (Employee) olarak tanımlanmış çalışanların ad ve soyad bilgilerini getirir. WHERE ifadesi, koşul belirleyerek filtreleme sağlar.
+----------------------------------------------------------------------
+🔹 2. İlk 10 Ürünün Adını ve Fiyatını Sıralayın
+  
+SELECT TOP 10 Name, ListPrice
+FROM Production.Product
+WHERE ListPrice > 0
+ORDER BY ListPrice DESC;
+
+Bu sorgu, liste fiyatı sıfırdan büyük olan ürünler arasından, en pahalı 10 tanesini TOP ifadesiyle seçer ve ORDER BY kullanılarak azalan düzende sıralar.
+
+---------------------------------------------------------------------------
