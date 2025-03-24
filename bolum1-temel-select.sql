591902a4dbf42419a85d618e228b89191cb2cbd3
thinkoptimize <thinkoptimize@yandex.com>
2025-03-24 11:33:37 +0300

    Create bolum1-temel-select.sql

---
+++ bolum1-temel-select.sql
@@ -0,0 +1,7 @@
+SELECT FirstName, LastName
+FROM Person.Person
+WHERE PersonType = 'EM';
+
+
+🎓 Açıklama:
+Bu sorgu, Person.Person tablosunda yer alan ve PersonType sütununda "EM" (Employee) olarak tanımlanmış çalışanların ad ve soyad bilgilerini getirir. WHERE ifadesi, koşul belirleyerek filtreleme sağlar.
